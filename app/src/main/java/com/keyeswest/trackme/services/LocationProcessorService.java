bcea765b07619a14ba4c3625af8d5653dbcfd012
Chip Keyes <chipk215@msn.com>
2018-05-18 10:08:00 -0600

    Documenting LocationProcessorService

--- app/src/main/java/com/keyeswest/trackme/services/LocationProcessorService.java
+++ app/src/main/java/com/keyeswest/trackme/services/LocationProcessorService.java
@@ -22,20 +22,35 @@ import static com.keyeswest.trackme.data.Queries.createNewLocationFromSample;
 import static java.lang.Math.max;
 import static java.lang.Math.min;
 
+/**
+ * This service receives location samples and the segment identifier corresponding to the segment
+ * that the locations belong to. Arriving Location samples are packaged in androids LocationResult
+ * objects.
+ *
+ * The service computes the bounding box for the location samples and the incremental segment
+ * distance increase associated with each location sample.
+ *
+ * After the segment computations have been made the segment data is updated in the database and
+ * each location sample is saved in the database.
+ *
+ * Finally, the location samples are broadcast back to the NewTripActivity for plotting on the map.
+ *
+ */
 public class LocationProcessorService extends IntentService {
 
     private final static String NAME="LocationProcessorService";
+
+    // Extra keys for accessing the location and segment data
     public final static String LOCATIONS_EXTRA_KEY = "locationsKey";
     public final static String SEGMENT_ID_EXTRA_KEY = "segmentIdKey";
 
+    // Intent and extra for sending locations to be plotted
     public final static String LOCATION_BROADCAST_PLOT_SAMPLE =
             "com.keyeswest.trackme.location.samples.intent.action.PLOT_SAMPLE";
-
     public final static String PLOT_SAMPLES_EXTRA_KEY = "sampleData";
 
-
+    // Debug counter  TODO remove from release
     public static long debugSampleCount =0;
-
 
 
     public LocationProcessorService() {
@@ -46,21 +61,19 @@ public class LocationProcessorService extends IntentService {
     /**
      * A new location sample (or samples) has arrived.
      *
-     * Generally, we want to save the location in the db and update the segment to which the
-     * location belongs.
-     *
-     * Updating the segment includes updating the segment distance by computing the distance
-     * the new location is from the last location sample (unless this is the first location of
-     * the segment, and then updating the bounding lat/lon box associated with the segment.
-     * @param intent
      */
     @Override
     protected void onHandleIntent(@Nullable Intent intent) {
+
         Double segmentDistance=0d;
 
+        // holds location returned from database
+        com.keyeswest.trackme.models.Location previousLocation;
 
         Timber.d("Entering LocationProcessorService onHandleIntent");
+
         if (intent != null) {
+            // retrieve the LocationResult data from intent
             LocationResult result = intent.getParcelableExtra(LOCATIONS_EXTRA_KEY);
 
             //get the location samples
@@ -71,32 +84,33 @@ public class LocationProcessorService extends IntentService {
                 String segmentId = intent.getStringExtra(SEGMENT_ID_EXTRA_KEY);
                 Timber.d("SegmentId= " + segmentId);
 
-                com.keyeswest.trackme.models.Location previousLocation = null;
 
                 // retrieve the last location belonging to this segment from the db
                 LocationCursor previousLocationCursor = Queries.getLatestLocationBySegmentId(
                         this, segmentId);
 
-                // bounding box for new samples
+                // save the location samples to the db and compute the bounding box
                 LatLonBounds bounds = saveLocationSamples(locations, segmentId);
 
-                // broadcast the location samples for plotting
-                broadcastLocationSamples(locations);
-
                 if (previousLocationCursor.getCount() == 1){
-                    // a previous location exists for the segment
+                    // a previous location exists for the segment, this is not hte first location
                     previousLocationCursor.moveToFirst();
                     previousLocation = previousLocationCursor.getLocation();
 
-                    Date d = new Date(previousLocation.getTimeStamp());
-                    Timber.d("Previous timestamp: " + d.toString() );
+                    //Date previousLocationData = new Date(previousLocation.getTimeStamp());
+                    //Timber.d("Previous timestamp: " + previousLocationData.toString() );
 
-                    // we are going to need to read the segment record
+                    // We are going to need to read the segment record
                     Segment segment = Queries.getSegmentFromSegmentId(this,segmentId);
 
                     bounds = adjustSegmentBounds(segment, bounds);
 
-                    // get the last location sample in this sample batch
+                    // Get the last location sample in this sample batch
+                    // this is a shortcut - revisit and loop through all the samples for a more
+                    // accurate calculation.
+                    // Also - opportunity to throw samples out (do not write to db) if their
+                    // incremental distance is less tan some threshold. Eliminate saving samples
+                    // where the user is at rest.
                     Location lastSample = locations.get(locations.size()-1);
                     // need the distance between lastSample and previousLocation
                     float[] results = new float[1];
@@ -126,12 +140,13 @@ public class LocationProcessorService extends IntentService {
                         bounds.getMinLon(), bounds.getMaxLon(),
                         segmentDistance);
 
+                // broadcast the location samples for plotting
+                broadcastLocationSamples(locations);
             }
         }
-
         Timber.d("Sample Count = %s", Long.toString(debugSampleCount));
-
     }
+
 
 
     private LatLonBounds adjustSegmentBounds(Segment segment, LatLonBounds bounds){
@@ -152,11 +167,19 @@ public class LocationProcessorService extends IntentService {
             bounds.setMaxLon(max(bounds.getMaxLon(), segment.getMaxLongitude()));
         }
 
-
         return bounds;
     }
 
 
+    /**
+     * Iterates over the location samples that came with the intent.
+     * Write the location samples to the database.
+     *
+     * Compute the bounding box which encapsulates the locations.
+     * @param locations
+     * @param segmentId
+     * @return
+     */
     private LatLonBounds saveLocationSamples(List<Location> locations, String segmentId){
 
         LatLonBounds bounds = new LatLonBounds();
@@ -180,14 +203,13 @@ public class LocationProcessorService extends IntentService {
             createNewLocationFromSample(this, l, segmentId);
 
             bounds.update(latitude, longitude);
-
         }
 
         return bounds;
-
     }
 
 
+    // Send the samples back to NewTrip activity to be plotted.
     private void broadcastLocationSamples(List<Location> locations){
         Intent intent = new Intent();
         intent.setAction(LOCATION_BROADCAST_PLOT_SAMPLE);
@@ -200,6 +222,5 @@ public class LocationProcessorService extends IntentService {
         LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
 
     }
-
 
 }
